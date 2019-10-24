9af5a6c3de23da65adb2b0d550e3fbbf489fde20
Kelvin Liang <kelvin9877@gmail.com>
2019-10-24 15:58:01 +0800

    fix some linter errors

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class KittensController < ApplicationController
   def index
     @kittens = Kitten.all
@@ -5,39 +7,37 @@ class KittensController < ApplicationController
 
   def show
     @kitten = Kitten.find_by(id: params[:format])
-    
   end
-  
+
   def new
     @kitten = Kitten.new
-    
   end
-  
+
   def create
-    @kitten = Kitten.new( name: params[:kitten][:name],
-                          age: params[:kitten][:age],
-                          cuteness: params[:kitten][:cuteness],
-                          softness: params[:kitten][:softness])
+    @kitten = Kitten.new(name: params[:kitten][:name],
+                         age: params[:kitten][:age],
+                         cuteness: params[:kitten][:cuteness],
+                         softness: params[:kitten][:softness])
     if @kitten.save
-      flash[:success] = "Kitten created!"
+      flash[:success] = 'Kitten created!'
       redirect_to kittens_path(@kitten.id)
     else
       render 'new'
     end
   end
-  
+
   def edit
     @kitten = Kitten.find_by(id: params[:format])
   end
-  
+
   def update
     @kitten = Kitten.find_by(id: params[:format])
 
     permitted = params.require(:kitten).permit(:name, :age, :cuteness, :softness)
     if @kitten.update_attributes(permitted)
-      flash[:success] = "Kitten edited!"
+      flash[:success] = 'Kitten edited!'
     else
-      flash[:warning] = "Kitten did not update"
+      flash[:warning] = 'Kitten did not update'
     end
 
     redirect_to kittens_path(@kitten.id)
@@ -46,8 +46,7 @@ class KittensController < ApplicationController
   def destroy
     @kitten = Kitten.find_by(id: params[:format])
     @kitten.destroy
-    flash[:success] = "Kitten deleted!"
+    flash[:success] = 'Kitten deleted!'
     redirect_to root_url
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  
   root to: 'kittens#index'
 
   get 'kittens/index', to: 'kittens#index'
   get 'kittens/show'
   # get 'kittens/new'
   # get 'kittens/edit'
-  
+
   resource :kittens
 end
